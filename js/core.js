c5dab1d50dbe1812d9a7a641086750b8a5171e0e
Aleksej Kozlov <aleksej.kozlov@gmail.com>
2014-05-10 22:33:33 +0300

    test

--- js/core.js
+++ js/core.js
@@ -2,16 +2,17 @@
     'use strict';
 
     var app = {
+        codes: {},
         $header: $('header'),
         $headerIcon: $('.header-icon-holder'),
         $mainWrapper: $('.main-wrapper'),
         $codeList: $('.code-list'),
         $editCodeList: $('.edit-code-list'),
+        editCodes: '.edit-code-item > input',
 
         init: function () {
-            this.buildCodeList(36);
-            this.buildEditList(36);
-            this.bindEvents();
+            //document.addEventListener('deviceready', this.onDeviceReady, false);
+            app.onDeviceReady(); // for web testing
         },
 
         buildEditList: function (count) {
@@ -20,7 +21,7 @@
                 inputWidth = w - 4 * gutter;
 
             for (var i = 1; i <= count; i++) {
-                var item = '<li><div class="edit-code-item"><label class="number" for="number_' + i + '">' + i + '</label><input class="code" type="number" name="number_' + i + '" id="number_' + i + '" style="width: ' + inputWidth + 'px"/></div></li>';
+                var item = '<li><div class="edit-code-item"><label class="number" for="number_' + i + '">' + i + '</label><input data-index="'+i+'" class="code" type="number" name="number_' + i + '" id="number_' + i + '" style="width: ' + inputWidth + 'px"/></div></li>';
 
                 app.$editCodeList.append(item);
             }
@@ -54,47 +55,62 @@
             window.plugins.clipboard.copy(code);
         },
 
-        // Bind Event Listeners
-        //
-        // Bind any events that are required on startup. Common events are:
-        // 'load', 'deviceready', 'offline', and 'online'.
-        bindEvents: function () {
-            /*
-            for (i = 0; i < this.numbers.length; i++) {
-                this.numbers[i].addEventListener('click', this.copyCode, false);
-            }
-            */
-            //document.addEventListener('deviceready', this.onDeviceReady, false);
-            
+        saveCodes: function() {
+            console.log('save all');
 
-            this.$headerIcon.on('click', function () {
-                $(this).toggleClass('settings');
-                app.$mainWrapper.toggleClass('active');
+            var codes = $(app.editCodes);
 
-                if (app.$mainWrapper.hasClass('active')) {
-                    //SoftKeyboard.show();
-                } else {
-                    //SoftKeyboard.hide();
-                }
+            $.each(codes, function () {
+                var $code = $(this),
+                    id = $code.data('index'),
+                    value = $code.val();
+                
+                console.log(id);
+                console.log(value);
+
+                app.codes[id] = value;
             });
-        },
-        // deviceready Event Handler
-        //
-        // The scope of 'this' is the event. In order to call the 'receivedEvent'
-        // function, we must explicity call 'app.receivedEvent(...);'
-        onDeviceReady: function() {
-            app.receivedEvent('deviceready');
-        },
-        // Update DOM on a Received Event
-        receivedEvent: function(id) {
-            var parentElement = document.getElementById(id);
-            var listeningElement = parentElement.querySelector('.listening');
-            var receivedElement = parentElement.querySelector('.received');
 
-            listeningElement.setAttribute('style', 'display:none;');
-            receivedElement.setAttribute('style', 'display:block;');
+            //console.log(app.codes);
+        },
 
-            console.log('Received Event: ' + id);
+        toggleEditCodeList: function() {
+            $(this).toggleClass('settings');
+            
+            if (app.$mainWrapper.hasClass('active')) {
+                app.saveCodes();
+            }
+
+            app.$mainWrapper.toggleClass('active');
+        },
+
+        bindEvents: function () {
+            this.$headerIcon.on('click', this.toggleEditCodeList);
+        },
+        /*
+        openDB: function () {
+            return window.openDatabase("Database", "0.0.1", "Copy Code", 200000);
+        },
+
+        populateDB: function (tx) {
+            //tx.executeSql('DROP TABLE IF EXISTS DEMO');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS codes (id INTEGER PRIMARY KEY, code)');
+        },
+
+        errorCB: function (err) {
+            alert("Error processing SQL: " + err.code);
+        },
+
+        successCB: function () {
+            var db = app.openDB();
+            db.transaction(app.queryDB, app.errorCB);
+        },
+        */
+        onDeviceReady: function () {
+            this.buildCodeList(36);
+            this.buildEditList(36);
+
+            this.bindEvents();   
         }
     }
 
